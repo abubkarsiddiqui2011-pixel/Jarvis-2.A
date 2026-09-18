afc44bfeeef7755987b4eb20bb64ef2469c78f24
ABUBKAR SIDDIKI <abubkarsiddiqui2011@gmail.com>
2026-09-18 18:14:34 +0530

    chore: remove project files and configuration
    Clean up repository by removing project-specific
    configuration, dependencies, and static assets.

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { JarvisDashboard } from "./components/JarvisDashboard";
-
-export default function App() {
-  return (
-    <div className="w-full h-screen">
-      <JarvisDashboard />
-    </div>
-  );
-}
